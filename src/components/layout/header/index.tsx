b90a6b17e1c76a687d797fd914ac792d77014d74
Júlio Pattuzzo <pattuzzo@protonmail.com>
2024-03-07 08:05:20 -0300

    feat: added area login in header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -118,94 +118,95 @@ export default function Header() {
       </Show>
 
       <div class={`
-      ${menuIsOpen() ? "block" : "hidden"}
+      ${menuIsOpen() ? "flex" : "hidden"}
+      flex-col justify-between items-center gap-2
       absolute top-[110%] right-0 z-1 w-100
       py-2 px-1 rounded-2xl
       bg-dark dark:bg-white
       `}>
-        <div class="flex flex-col gap-1">
-          <div class="flex flex-col gap-2">
-            <span class="w-100 flex justify-between items-center">
-              <span class={`${menuLanguageisOpen() && "bg-white dark:bg-dark"} relative flex items-center rounded-lg p-0.25 transition-none`}>
-                <button
-                  class=""
-                  onClick={() => setMenuLanguageIsOpen(!menuLanguageisOpen())}
-                >
-                  <img class="size-2.5" src={flag()} alt="" />
-                </button>
-                <div
-                  class={
-                    `${menuLanguageisOpen() ? "flex" : "hidden"}
-                    absolute top-[78%] left-50 -translate-x-50 w-100
-                    flex-col items-center gap-0
-                    rounded-lg p-0.25 bg-white dark:bg-dark
-                    `}
-                >
-                  <button
-                    title="Portuguese"
-                    class={`${flag().includes("/br") && "hidden"}`}
-                    onClick={() => {
-                      setLanguage("pt");
-                      setFlag(flags.pt);
-                      setMenuLanguageIsOpen(!menuLanguageisOpen());
-                    }}
-                  >
-                    <img class="size-2.5" src={flags['pt']} alt="" />
-                  </button>
-                  <button
-                    title="English"
-                    class={`${flag().includes("/en") && "hidden"}`}
-                    onClick={() => {
-                      setLanguage("en");
-                      setFlag(flags.en);
-                      setMenuLanguageIsOpen(!menuLanguageisOpen());
-                    }}
-                  >
-                    <img class="size-2.5" src={flags['en']} alt="" />
-                  </button>
-                  <button
-                    title="Spanish"
-                    class={`${flag().includes("/sp") && "hidden"}`}
-                    onClick={() => {
-                      setLanguage("es");
-                      setFlag(flags.es);
-                      setMenuLanguageIsOpen(!menuLanguageisOpen());
-                    }}
-                  >
-                    <img class="size-2.5" src={flags['es']} alt="" />
-                  </button>
-                </div>
-              </span>
-              <a class="
-              flex items-center justify-center
-              p-0.375 px-0.75
-              dark:rounded-full
-              text-3xl
-              text-white dark:text-black
-              " href="/">
-                <Logo class="size-2.5 fill-white dark:fill-black" />
-                FutureSphere
-              </a>
-              <Show
-                when={theme() == "light"} fallback={
-                  <button title="dark" class="inline-block rounded-full dark:text-black" onClick={() => setTheme("light")}>
-                    <Icon class="inline-block" color="fill-yellow-500  stroke-yellow-500 size-2.5" name="FiMoon"/>
-                  </button>
-                }>
-                <button title="light" class="inline-block rounded-full dark:text-black" onClick={() => setTheme("dark")}>
-                  <Icon class="inline-block" color="fill-yellow-500 stroke-yellow-500 size-2.5" name="FiSun"/>
-                </button>
-              </Show>
-            </span>
-            <nav class="flex flex-col justify-center items-center gap-1.25 text-white dark:text-black">
-              <A class="text-xl hover:text-purple" activeClass="text-purple" href="/about" end>{t("header.menu.about")}</A>
-              <A class="text-xl hover:text-purple" activeClass="text-purple" href="/careers" end>{t("header.menu.careers")}</A>
-              <A class="text-xl hover:text-purple" activeClass="text-purple" href="/blogs" end>{t("header.menu.blogs")}</A>
-              <A class="text-xl hover:text-purple" activeClass="text-purple" href="/pricing" end>{t("header.menu.pricing")}</A>
-              <A class="text-xl hover:text-purple" activeClass="text-purple" href="/pricing" end>{t("header.menu.contact")}</A>
-            </nav>
-          </div>
-        </div>
+        <span class="w-100 flex justify-between items-center">
+          <span class={`${menuLanguageisOpen() && "bg-white dark:bg-dark"} relative flex items-center rounded-lg p-0.25 transition-none`}>
+            <button
+              class=""
+              onClick={() => setMenuLanguageIsOpen(!menuLanguageisOpen())}
+            >
+              <img class="size-2.5" src={flag()} alt="" />
+            </button>
+            <div
+              class={
+                `${menuLanguageisOpen() ? "flex" : "hidden"}
+                absolute top-[78%] left-50 -translate-x-50 w-100
+                flex-col items-center gap-0
+                rounded-lg p-0.25 bg-white dark:bg-dark
+                `}
+            >
+              <button
+                title="Portuguese"
+                class={`${flag().includes("/br") && "hidden"}`}
+                onClick={() => {
+                  setLanguage("pt");
+                  setFlag(flags.pt);
+                  setMenuLanguageIsOpen(!menuLanguageisOpen());
+                }}
+              >
+                <img class="size-2.5" src={flags['pt']} alt="" />
+              </button>
+              <button
+                title="English"
+                class={`${flag().includes("/en") && "hidden"}`}
+                onClick={() => {
+                  setLanguage("en");
+                  setFlag(flags.en);
+                  setMenuLanguageIsOpen(!menuLanguageisOpen());
+                }}
+              >
+                <img class="size-2.5" src={flags['en']} alt="" />
+              </button>
+              <button
+                title="Spanish"
+                class={`${flag().includes("/sp") && "hidden"}`}
+                onClick={() => {
+                  setLanguage("es");
+                  setFlag(flags.es);
+                  setMenuLanguageIsOpen(!menuLanguageisOpen());
+                }}
+              >
+                <img class="size-2.5" src={flags['es']} alt="" />
+              </button>
+            </div>
+          </span>
+          <a class="
+          flex items-center justify-center
+          p-0.375 px-0.75
+          dark:rounded-full
+          text-3xl
+          text-white dark:text-black
+          " href="/">
+            <Logo class="size-2.5 fill-white dark:fill-black" />
+            FutureSphere
+          </a>
+          <Show
+            when={theme() == "light"} fallback={
+              <button title="dark" class="inline-block rounded-full dark:text-black" onClick={() => setTheme("light")}>
+                <Icon class="inline-block" color="fill-yellow-500  stroke-yellow-500 size-2.5" name="FiMoon"/>
+              </button>
+            }>
+            <button title="light" class="inline-block rounded-full dark:text-black" onClick={() => setTheme("dark")}>
+              <Icon class="inline-block" color="fill-yellow-500 stroke-yellow-500 size-2.5" name="FiSun"/>
+            </button>
+          </Show>
+        </span>
+        <nav class="flex flex-col justify-center items-center gap-1.25 text-white dark:text-black">
+          <A class="text-xl hover:text-purple" activeClass="text-purple" href="/about" end>{t("header.menu.about")}</A>
+          <A class="text-xl hover:text-purple" activeClass="text-purple" href="/careers" end>{t("header.menu.careers")}</A>
+          <A class="text-xl hover:text-purple" activeClass="text-purple" href="/blogs" end>{t("header.menu.blogs")}</A>
+          <A class="text-xl hover:text-purple" activeClass="text-purple" href="/pricing" end>{t("header.menu.pricing")}</A>
+          <A class="text-xl hover:text-purple" activeClass="text-purple" href="/pricing" end>{t("header.menu.contact")}</A>
+        </nav>
+        <span class="flex flex-col items-center gap-0.5 w-full">
+          <a class="w-full text-center rounded-full px-1.25 py-0.375 text-white bg-purple" href="">{t("ui.login")}</a>
+          <a class="w-full text-center rounded-full px-1.25 py-0.375 text-black dark:text-white bg-white dark:bg-dark" href="">{t("ui.signup")}</a>
+        </span>
       </div>
     </header>
   );
